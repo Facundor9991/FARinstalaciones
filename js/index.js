94d0a29291b2035436d38a143fab247c1c7acb43
Facundor9991 <facundor99@outlook.com>
2023-02-17 23:28:36 -0300

    se agrego nuevo proyecto

---
+++ js/index.js
@@ -0,0 +1,10 @@
+Swal.fire({
+    title: "Te doy la bienvenida a FAR",
+    text: "Nosotros instalamos tu equipo de aire para que lo disfrutes",
+    //icon: 'error',
+    confirmButtonText: "Entrar",
+    confirmButtonColor: "green"
+  
+  })
+  
+  
